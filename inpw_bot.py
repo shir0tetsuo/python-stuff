14c759415a2bcde6d89f281abf9f2e6e5d084701
shir0tetsuo <shadowsword@protonmail.com>
2023-04-04 10:03:50 -0400

    1.1 Several bugfix and QoL

--- inpw_bot.py
+++ inpw_bot.py
@@ -1,11 +1,18 @@
 import discord
 from discord.ext import commands
+import random
 import json
 import os
 import time
 from datetime import timedelta
 import re
 from hashlib import sha256
+# just a discord_token.py file with bot_token string
+import discord_token
+bot_token = discord_token.bot_token
+
+shadow_folder = '.inpw'
+'''This is the folder where all of the .json will be saved.'''
 
 # Check if the Operating System is Windows(NT) or Linux(Posix) then make a path to a hidden folder with the chains in it.
 if os.name == 'nt':
@@ -13,12 +20,20 @@ if os.name == 'nt':
     main_path.remove(main_path[-1])
     main_path = '\\'.join(main_path) + '\\'
     # The hidden folder with your chains.
-    main_folder = '{}.avaira'.format(main_path)
+    main_folder = '{}{}'.format(main_path, shadow_folder)
     '''The place where all chain .json are stored.'''
 else:
     main_path = os.getcwd() + '/'
-    main_folder = '{}.avaira'.format(main_path)
+    main_folder = '{}{}'.format(main_path, shadow_folder)
 
+# MODIFY FOR YOUR PURPOSES.
+
+bot_handler_users = [
+    '303309686264954881'
+]
+'''Discord userIDs list.'''
+
+# Shouldn't modify unless needed.
 if not os.path.exists(main_folder):
     os.makedirs(main_folder)
 os.chdir(main_folder)
@@ -35,11 +50,13 @@ with open(this_file,"r") as monitoring_script:
     this_hash = sha256(this_dump).hexdigest()
 this_modified_time = time.ctime(os.path.getmtime(this_file))
 
-
-
 # This will appear in every Genesis Block and is required for pristine_genesis.
 genesis_signature = 'Genesis Block [shadowsword#0179] VERSION {} {}'.format(this_hash, this_modified_time) # or "Genesis"
-'''Genesis Block Signature'''
+'''Genesis Block Signature, this script's hash and modification time are included'''
+
+#
+# eof out-of-box lines
+# 
 
 # a generalized exception
 class GenesisOnly(Exception):
@@ -54,29 +71,42 @@ def Get():
         print('{}: {} Loaded'.format(idx, ch.name))
     return zones
 
+def RandomHex():
+    return hex(random.randint(0,65536))
 #
 #
 # THE DISCORD SIDE OF THINGS
 #
 #
 
+def command_help():
+    '''The Discord-side help command execution, returns prepared string.'''
+    help_lines = [
+                    '`in` inspects or appends to block in chunk ||in 0x0 :black_circle: test||',
+                    '`list` displays the chunks in list ||list 0x0 (int(chunk-index))||',
+                    '`trail` inspects the block index in chunk ||trail 0x0 0||',
+                    '`rm` removes the chunk ||rm 0x0||',
+                    '`--random` returns hex value between 0 and 65536',
+                    '`--help` <- you are here'
+                ]
+    return '\n'.join(help_lines)
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}! use --help to display help.')
 
     async def on_message(self, message):
-        if (str(message.author.id) == '303309686264954881'):
+        if (str(message.author.id) in bot_handler_users):
 
+            # --HELP
             if message.content.split(' ')[0].lower() == '--help':
-                help_lines = [
-                    '`in` inspects or appends to block in chunk ||in 0x00 :black_circle: test||',
-                    '`list` displays the chunks in list',
-                    '`trail` inspects the block index in chunk ||trail 0x00 0||',
-                    '`rm` removes the chunk ||rm 0x00||',
-                    '`--help` <- you are here'
-                ]
-                return await message.channel.send('\n'.join(help_lines))
+                return await message.channel.send(command_help())
 
+            # GETRANDHEX
+            if message.content.split(' ')[0].lower() == '--random':
+                return await message.channel.send('`{}`'.format(RandomHex()))
+
+            # TRAIL
             if message.content.split(' ')[0].lower() == 'trail':
                 args = message.content.split(' ')
                 if len(args) < 3:
@@ -94,19 +124,24 @@ class MyClient(discord.Client):
                 except ValueError:
                     return await message.channel.send('Index Unknown')
                 
-                F = Floor(hex_index = args[1])
+                F = Floor(hex_index = args[1].lower())
                 B = F.chain[int(args[2])]
 
                 pristine = ':green_circle:' if F.pristine else ':red_circle:'
+                pristine_diff = ':green_circle:' if F.pristine_difficulty else ':red_circle:'
+                pristine_gen = ':green_circle:' if F.pristine_genesis else ':red_circle:'
                 data = [
-                        '__`{}`__ ({}) {}'.format(F.hex_index, F.numerical, pristine),
+                        '__`{}`__ ({})'.format(F.hex_index, F.numerical, pristine),
                         '> #{} {} "{}"'.format(B.index, B.symbol, B.comment),
                         '> `{}`'.format(B.compute_hash()),
+                        '',
+                        '> `history` {} `difficulty` {} `genesis version` {} '.format(pristine, pristine_diff, pristine_gen),
+                        '> `prev_hash {}`'.format(B.previous_hash),
                         '{} (since: {})'.format(time.ctime(B.timestamp), F.time_gap)
                     ]
                 return await message.channel.send('\n'.join(data))
 
-
+            # RM
             if message.content.split(' ')[0].lower() == 'rm':
                 args = message.content.split(' ')
                 if len(args) < 2:
@@ -117,9 +152,10 @@ class MyClient(discord.Client):
                 except ValueError:
                     return
                 
-                Floor(hex_index = args[1]).delete()
+                Floor(hex_index = args[1].lower()).delete()
                 return await message.channel.send('Deleted {}'.format(args[1]))
 
+            # IN
             if message.content.split(' ')[0].lower() == 'in':
                 args = message.content.split(' ')
                 if len(args) < 2:
@@ -128,18 +164,22 @@ class MyClient(discord.Client):
                 try: 
                     int(args[1], base=16)
                 except ValueError:
-                    return await message.channel.send('Not base 16')
+                    return
 
-                chain = Floor(hex_index = args[1])
+                chain = Floor(hex_index = args[1].lower())
                 last_block = chain.last_block
 
                 if len(args) == 2:
-                    pristine = ':green_circle:' if chain.pristine else ':red_circle:'
+                    pristine = ':scroll:' if chain.pristine else ':fire:'
+                    pristine_diff = ':chains:' if chain.pristine_difficulty else ':wrench:'
+                    pristine_gen = ':milky_way:' if chain.pristine_genesis else ':city_sunset:'
                     data = [
-                        '__`{}`__ ({}) {}'.format(chain.hex_index, chain.numerical, pristine),
+                        '__`{}`__ ({})'.format(chain.hex_index, chain.numerical),
                         '> #{} {} "{}"'.format(last_block.index, last_block.symbol, last_block.comment),
                         '> `{}`'.format(last_block.compute_hash()),
-                        '{} (since: {})'.format(time.ctime(last_block.timestamp), chain.time_gap)
+                        '> :lab_coat: ||{}{}{}||'.format(pristine, pristine_diff, pristine_gen),
+                        '{} (since: {})'.format(time.ctime(last_block.timestamp), chain.time_gap),
+                        
                     ]
                     return await message.channel.send('\n'.join(data))
                 
@@ -149,9 +189,10 @@ class MyClient(discord.Client):
                     comment = message.content[trail_position:]
                     if not comment:
                         comment = ""
-                    Floor(hex_index = args[1]).add(symbol, comment)
+                    Floor(hex_index = args[1].lower()).add(symbol, comment)
                     return await message.channel.send("Data added.")
 
+            # LIST
             if message.content.split(' ')[0].lower() == 'list':
                 args = message.content.split(' ')
                 if len(args) < 2:
@@ -163,16 +204,43 @@ class MyClient(discord.Client):
                     return await message.channel.send('Not base 16')
                 
                 # start, stop, step 1
-                lowest = point - 5
-                highest = point + 6
+                lowest = point - 8
+                highest = point + 9
                 all_pt = []
-                for index in reversed(range(lowest, highest, 1)):
-                    pt = Floor(hex_index = hex(index))
-                    if index == point:
-                        all_pt.append('> -> {} `{}` "{}"'.format(pt.last_block.symbol, pt.hex_index, pt.last_block.comment))
-                    else:
-                        all_pt.append('> {} `{}`'.format(pt.last_block.symbol, pt.hex_index))
-                return await message.channel.send('\n'.join(all_pt))
+
+                try:
+                    index_argument = int(args[2])
+                except Exception:
+                    index_argument = -1
+                
+                if index_argument != -1:
+                    for index_chunk in reversed(range(lowest, highest, 1)):
+                        pt = Floor(hex_index = hex(index_chunk))
+                        try:
+                            chunk = pt.chain[index_argument]
+                        except Exception:
+                            chunk = pt.last_block
+                        if index_chunk == point:
+                            all_pt.append('')
+                            all_pt.append('> <- {} `{}` `#{}` {}'.format(chunk.symbol, pt.hex_index, chunk.index, chunk.comment))
+                            all_pt.append('> -> {} `{}` `#{}` {}'.format(pt.last_block.symbol, pt.hex_index, pt.last_block.index, pt.last_block.comment))
+                            delta_point = str(timedelta(seconds=(pt.last_block.timestamp - chunk.timestamp)))
+                            all_pt.append('> <--> `[{}]` ({} & {})'.format(delta_point, time.ctime(chunk.timestamp), time.ctime(pt.last_block.timestamp)))
+                            all_pt.append('')
+                        else:
+                            all_pt.append('> {} `{}` `#{}`'.format(chunk.symbol, pt.hex_index, chunk.index))
+                    
+                    all_pt.append('`trail mode`')
+                    return await message.channel.send('\n'.join(all_pt))
+
+                else:
+                    for index in reversed(range(lowest, highest, 1)):
+                        pt = Floor(hex_index = hex(index))
+                        if index == point:
+                            all_pt.append('> -> {} `{}` `#{}` {}'.format(pt.last_block.symbol, pt.hex_index, pt.last_block.index, pt.last_block.comment))
+                        else:
+                            all_pt.append('> {} `{}` `#{}`'.format(pt.last_block.symbol, pt.hex_index, pt.last_block.index))
+                    return await message.channel.send('\n'.join(all_pt))
 
             pass
         #print(f'Message from {message.author}: {message.content}')
@@ -272,6 +340,19 @@ class Floor:
                     verified = False
         return verified
     
+    @property
+    def pristine_genesis(self):
+        return True if self.chain[0].comment == genesis_signature else False
+    
+    @property
+    def pristine_difficulty(self):
+        verified = True
+        for index in reversed(range(len(self.chain))):
+            if index > 0:
+                if not self.chain[index].compute_hash().startswith('0' * Floor.difficulty):
+                    verified = False
+        return verified
+    
     def proof_of_work(self, block):
         block.nonce = 0
         computed_hash = block.compute_hash()
@@ -307,4 +388,4 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run('') # YOUR BOT KEY
+client.run(bot_token)
